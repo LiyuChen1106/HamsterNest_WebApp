6693949000d1d71bc1ba53086e83de5de37adeed
joey <joeychen@github.com>
2018-10-12 13:56:29 -0400

    rename table user_information to user_detail

--- hamsterNest/config/routes.rb
+++ hamsterNest/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :items
+  resources :user_details
+  resources :users
+  resources :histories
+  resources :categories
+
+
 end
