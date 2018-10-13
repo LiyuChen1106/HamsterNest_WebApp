abf3024ba7b5e2a97ba61edf55c6ebd698d835d6
Liyu Chen <esther.chen@mail.utoronto.ca>
2018-10-13 19:02:19 -0400

    Initial backbone for registering new accounts. issue: https://github.com/LiyuChen1106/HamsterNest_WebApp/projects/1#card-13827867

--- hamsterNest/app/controllers/user_accounts_controller.rb
+++ hamsterNest/app/controllers/user_accounts_controller.rb
@@ -1,2 +1,46 @@
 class UserAccountsController < ApplicationController
+  
+  def index
+    @user_accounts = UserAccount.all
+  end
+  
+  def show
+    @user_account = UserAccount.find(params[:id])
+  end
+  
+  def new
+    @user_account = UserAccount.new
+  end
+  
+  def edit
+    @user_account = UserAccount.find(params[:id])
+  end
+  
+  def create
+    @user_account = UserAccount.new(params.require(:user_account).permit(:email, :password))
+    
+    if @user_account.save
+      flash[:notice] = "Signup Sucessfully"
+      # render to fill up their user information
+      redirect_to :root
+    else
+      flash[:alert] = "Problem"
+      render new
+    end
+  end
+  
+  def update
+	  @user_account = UserAccount.find(params[:id])
+ 
+  	if @user_account.update(account_params)
+    	redirect_to @user_account
+  	else
+    	render 'edit'
+  	end
+	end
+  
+  private
+    def account_params
+      params.require(:user_account).permit(:email, :password)
+    end
 end
