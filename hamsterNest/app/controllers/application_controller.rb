2b015c868906c2136abf752cc753ef07964ba4d4
caiyuxi <yuxijune.cai@mail.utoronto.ca>
2018-11-14 21:20:07 -0500

    Added top banner to all pages, added css definition to item form, reorganized root page layout

--- hamsterNest/app/controllers/application_controller.rb
+++ hamsterNest/app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < ActionController::Base
-#  def show
-#    render params[:page]
-#  end
-#  
-  
-  
-  # for Devise
+  before_action :set_search
+
   def after_sign_in_path_for(resource)
     if current_user.sign_in_count == 1
       new_user_profile_path
@@ -13,8 +8,16 @@ class ApplicationController < ActionController::Base
       root_path
     end
   end
-  
+
+
   def after_sign_out_path_for(resource)
       root_path
   end
+
+
+  def set_search
+    @search = Item.ransack(params[:q])
+  end
+
+
 end
