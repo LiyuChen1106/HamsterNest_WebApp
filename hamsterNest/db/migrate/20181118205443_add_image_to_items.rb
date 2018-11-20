5ca4b1d87bc79d71e29628235838657785337ae6
caiyuxi <yuxijune.cai@mail.utoronto.ca>
2018-11-19 23:10:18 -0500

    remove migration files of unused fields

--- hamsterNest/db/migrate/20181118205443_add_image_to_items.rb
+++
@@ -1,9 +0,0 @@
-class AddImageToItems < ActiveRecord::Migration[5.2]
-  def up
-    add_attachment :items, :image
-  end
- 
-  def down
-    remove_attachment :items, :image
-  end
-end
--- hamsterNest/db/migrate/20181118205938_add_avatar_to_user_profiles.rb
+++
@@ -1,9 +0,0 @@
-class AddAvatarToUserProfiles < ActiveRecord::Migration[5.2]
-  def up
-    add_attachment :user_profiles, :avatar
-  end
-
-  def down
-    remove_attachment :user_profiles, :avatar
-  end
-end
