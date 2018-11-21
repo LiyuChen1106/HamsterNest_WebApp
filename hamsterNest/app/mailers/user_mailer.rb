063d8c494a6e202f281d8a5b857354b5a40bd5cd
Liyu Chen <esther.chen@mail.utoronto.ca>
2018-11-20 23:44:53 -0500

    Added approve/reject borrow request email

--- hamsterNest/app/mailers/user_mailer.rb
+++ hamsterNest/app/mailers/user_mailer.rb
@@ -16,12 +16,33 @@ class UserMailer < ApplicationMailer
   end
   
   def borrow_request_confirmation_email
-    @lender = params[:lender].user
+    @lender = params[:lender]
     @borrower = params[:borrower]
     @item = params[:item]
     @borrow_request = params[:borrow_request]
     @url = item_borrow_request_url(:item_id => @item.id, :id => @borrow_request.id)
     
     mail(to: @lender.email, subject: 'Borrow request on your item')
+  end
+  
+  def borrow_request_approved_email
+    @lender = params[:lender]
+    @borrower = params[:borrower]
+    @item = params[:item]
+    @borrow_request = params[:borrow_request]
+    @url = item_url(:id => @item.id)
+    
+    mail(to: @borrower.email, subject: 'Approved borrow request')
+  end
+  
+  def borrow_request_rejected_email
+    @lender = params[:lender]
+    @borrower = params[:borrower]
+    @item = params[:item]
+    @borrow_request = params[:borrow_request]
+    @item_name = @item.item_name
+    @url = item_url(:id => @item.id)
+    
+    mail(to: @borrower.email, subject: 'Rejected borrow request')
   end
 end
