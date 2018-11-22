9fe3f313227f93aec3fdd8bb5161d6a2b0d2c3d2
Liyu Chen <esther.chen@mail.utoronto.ca>
2018-11-22 17:21:41 -0500

    Changed error color to red

--- hamsterNest/app/helpers/application_helper.rb
+++ hamsterNest/app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def bootstrap_class_for flash_type
-    { success: "alert-success", error: "alert-error", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
 end
 
   def flash_messages(opts = {})
