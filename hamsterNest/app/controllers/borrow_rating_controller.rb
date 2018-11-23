4e0095f0d4946b60aec04e4f57346ef0a813ffe8
Kaishan <kaishan.zhang@mail.utoronto.ca>
2018-11-23 02:49:19 -0500

    rating controller

---
+++ hamsterNest/app/controllers/borrow_rating_controller.rb
@@ -0,0 +1,29 @@
+class BorrowRatingController < ApplicationController
+
+
+    def edit
+        @borrow_person=UserProfile.find(params[:id])
+    end
+    def update
+        @borrow_person=UserProfile.find(params[:id])
+        @b_rating=@borrow_person[:borrow_rating]
+        @b_people=@borrow_person[:bpeople]
+        @b_people=@b_people+1
+        @b_rating=(@b_rating+params[:rating])/@bpeople
+        :borrow_rating => @b_rating
+        :bpeople => @b_people
+        if @borrow_person.update(rate_params)
+            redirect_to current_user
+        else
+            render 'edit'
+        end
+    end
+
+    private
+    def rate_params
+        params.require(:user_profile).permit(:borrow_rating, :bpeople)
+    end
+
+
+
+end
