e2bf7c3e499cc185e30b7d10cdedaf4a6fb52acc
Jinyi <jinyi.chen@mail.utoronto.ca>
2018-11-27 17:19:52 -0500

    resolve conflicts

--- hamsterNest/db/schema.rb
+++ hamsterNest/db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_11_27_212927) do
-=======
 ActiveRecord::Schema.define(version: 2018_11_27_220138) do
->>>>>>> 6b725c86bf5c996c62a17fc8b4fae609e29757c7
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
