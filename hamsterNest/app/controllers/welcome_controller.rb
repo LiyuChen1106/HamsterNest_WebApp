1e649447ff3b76ad88ff7fb696df4dfc96ce2b00
Jinyi <jinyi.chen@mail.utoronto.ca>
2018-11-26 21:25:04 -0500

    skip authetication for new introduction pages

--- hamsterNest/app/controllers/welcome_controller.rb
+++ hamsterNest/app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  skip_before_action :authenticate_user!, :only => [:index]
+  skip_before_action :authenticate_user!
 
   def index
     d = Time.now
