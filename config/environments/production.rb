d5521216085dbe7bb708f984634c29447e396424
Liyu Chen <esther.chen@mail.utoronto.ca>
2018-12-02 00:13:44 -0500

    Force all access to https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,4 +106,8 @@ Rails.application.configure do
     :domain               =>  'heroku.com',
     :enable_starttls_auto  =>  true
   }
+  
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
 end
