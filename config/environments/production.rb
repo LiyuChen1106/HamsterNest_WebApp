177a0dab2f5bce79e326b91badd6cb5e0e3084c9
Liyu Chen <esther.chen@mail.utoronto.ca>
2018-12-02 00:38:11 -0500

    Changed domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   # Mailer
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'hamster-nest.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'hamster-nest.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address              =>  'smtp.sendgrid.net',
@@ -103,7 +103,7 @@ Rails.application.configure do
     :authentication       =>  :plain,
     :user_name            =>  ENV['SENDGRID_USERNAME'],
     :password             =>  ENV['SENDGRID_PASSWORD'],
-    :domain               =>  'heroku.com',
+    :domain               =>  'herokuapp.com',
     :enable_starttls_auto  =>  true
   }
   
