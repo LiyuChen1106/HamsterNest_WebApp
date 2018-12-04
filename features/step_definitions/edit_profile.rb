05c8cde7d357151516b27b589790e97d4634fa0b
Khoi To <khoi.to@mail.utoronto.ca>
2018-12-04 03:44:51 -0800

    Added edit cucumber test

---
+++ features/step_definitions/edit_profile.rb
@@ -0,0 +1,16 @@
+Then (/^I edit profile$/) do
+    # visit ('/account/profiles/' << $user.user_profile_id)
+    # click_button('Edit My Profile')
+    # fill_in('user_profile_username', :with => 'rebelteen')
+    
+    # fill_in('user_profile_first_name', :with => 'dante')
+    
+    # fill_in('user_profile_last_name', :with => 'kennedy')
+    
+    # fill_in('user_profile_address_street_address', :with => '33 Joe Street')
+    
+    # fill_in('user_profile_address_city', :with => 'Etobicoke')
+    
+    # fill_in('user_profile_address_postal_code', :with => 'M6A 7A3')
+    # click_button('Submit')
+end
